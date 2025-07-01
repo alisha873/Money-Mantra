6083a56b3c33958ebbc586994d08aea911202e0b
alisha873 <ali07062005@gmail.com>
2025-07-01 18:32:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app=express();
 
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://127.0.0.1:8080', 'http://localhost:5500', 'http://localhost:8080'],
+    origin: ['http://127.0.0.1:5500', 'http://127.0.0.1:8080', 'http://localhost:5500', 'http://localhost:8080','https://moneymantraa.vercel.app'],
     credentials: true
 }));
 //middlewares - to parse data and handle form requests
@@ -15,6 +15,10 @@ app.use(express.urlencoded({extended:true}));
 
 //routes
 app.use('/api',routes);
+
+app.get('/', (req, res) => {
+  res.send('Money Mantra backend is running!');
+});
 
 const PORT= 3000;
 app.listen(PORT,()=>{
