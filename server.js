a53fd3f6188d2bdb7b68f4ff779cc4af0e8981b0
alisha873 <ali07062005@gmail.com>
2025-07-04 22:04:41 +0530

    Initial commit for local-code branch

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(cors({
     origin: ['http://127.0.0.1:5500', 'http://127.0.0.1:8080', 'http://localhost:5500', 'http://localhost:8080'],
     credentials: true
 }));
+
 //middlewares - to parse data and handle form requests
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
