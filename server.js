5ab9c87cbc4dae08cfe0d285b435cec1b0df5e1c
alisha873 <ali07062005@gmail.com>
2025-07-11 19:08:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
-const express = require('express');
-const app = express();
+const express = require ('express');
+const http = require('http');
+const routes=require('./routes.js')
+const cors = require('cors');
+
+const app=express();
+
+app.use(cors({
+  origin: ['https://moneymatra.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+//middlewares - to parse data and handle form requests
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//routes
+app.use('/api',routes);
 
 app.get('/', (req, res) => {
   res.send('Money Mantra backend is running!');
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`);
+const PORT= process.env.PORT || 8080 ;
+app.listen(PORT,()=>{
+    console.log(`Server running on PORT ${PORT}`);
 });
